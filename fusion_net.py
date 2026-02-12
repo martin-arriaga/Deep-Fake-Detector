ed2bee72e76dcbb0e73a89042bcdc975e125823a
martin-arriaga <martinarriaga@yahoo.com>
2026-02-12 14:03:30 -0600

    fusion net update

--- fusion_net.py
+++ fusion_net.py
@@ -1,8 +1,39 @@
 import torch
 import torch.nn as nn
-from torchvison import models
+from torchvision import models
 
 class FusionNet(nn.Module):
     def __init__(self):
         super(FusionNet, self).__init__()
+
+        self.spatial = models.efficientnet_b0(pretrained=True)
+        self.spatial.classifier = nn.Identity()
+        """
+        Create our own CCN 
+        """
+        self.frequency_branch = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Flatten()
+
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Linear(1280 + 64, 512),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(512, 1),
+            nn.Sigmoid()
+        )
+
+        def forward(self, x, dct):
+            features_x = self.spatial(x)
+            features_xdct = self.frequency_branch(dct)
+            concat = torch.cat((features_x, features_xdct), 1)
+            return self.classifier(concat)
+
 
